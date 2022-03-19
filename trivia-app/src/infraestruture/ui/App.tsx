de177ad6df5ebd403b8a33adf1b6f85b2ba6bc36
Kevin Kuhn <kmulinarik@hotmail.com>
2022-03-19 19:17:23 -0300

    fix browser router

--- trivia-app/src/infraestruture/ui/App.tsx
+++ trivia-app/src/infraestruture/ui/App.tsx
@@ -1,15 +1,13 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { HomeScreen, QuizScreen, ResultsScreen } from './screens'
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/quiz/:id" element={<QuizScreen />} />
-        <Route path="/quiz/results" element={<ResultsScreen />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/quiz/:id" element={<QuizScreen />} />
+      <Route path="/quiz/results" element={<ResultsScreen />} />
+    </Routes>
   )
 }
 
--- trivia-app/src/main.tsx
+++ trivia-app/src/main.tsx
@@ -2,16 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './infraestruture/ui/App'
 
+import { BrowserRouter } from 'react-router-dom'
+
 import { TriviaProvider } from './infraestruture/contexts/TriviaContext'
 import { ErrorProvider } from './infraestruture/contexts/ErrorContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <TriviaProvider>
-      <ErrorProvider>
-        <App />
-      </ErrorProvider>
-    </TriviaProvider>
+    <BrowserRouter>
+      <TriviaProvider>
+        <ErrorProvider>
+          <App />
+        </ErrorProvider>
+      </TriviaProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
